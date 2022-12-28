97511c5e7ff9a700ada680674469093e023f825c
Mohammod Ali <mohammodali651@gmail.com>
2022-12-29 01:58:30 +0600

    post data load function added

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 const app = express()
 
+// middleware
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7haskyh.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        const postCollection = client.db('mediaBook').collection('postCollection')
+
+        app.get('/post', async(req, res) => {
+            const query = {}
+            const options = await postCollection.find(query).toArray()
+            res.send(options)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.log)
 
 app.get('/', async(req, res) => {
     res.send('MediaBook server is running')
